b3cbeea21fd58b0e26b20980a2d8b965b2cd8ae2
lucas pietro <cursoads00@gmail.com>
2025-03-19 20:31:13 -0300

    Update User.java

--- hojeondetest-main/hoje/src/main/java/com/hojeonde/hoje/models/User.java
+++ hojeondetest-main/hoje/src/main/java/com/hojeonde/hoje/models/User.java
@@ -9,13 +9,13 @@ import lombok.Setter;
 import java.util.UUID;
 
 @Entity
-@Table
+@Table (name = "Users")
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
     @Id
-    @GeneratedValue (strategy = GenerationType.AUTO)
+ @GeneratedValue(strategy = GenerationType.UUID)
     private UUID idUser;
     private String cpf;
     private String nome;
